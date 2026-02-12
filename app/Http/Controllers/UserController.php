d214d81491d3d05090c90c8408838a035d5f4fd0
Pub De <ppubbuddu@gmail.com>
2026-02-12 10:54:29 +0530

    feat(auth): add login rate limiting
    Add rate limit for login to prevent the brute force attack

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\RateLimiter;
 
 
 class UserController extends Controller
@@ -52,26 +52,48 @@ class UserController extends Controller
     public function login(Request $request){
 
         try {
+            
             $request->validate([
-                    'email' => 'required|email',
-                    'password' => 'required',
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+
+            $key =$request->email."|".$request->ip();
+
+
+            // Allow max 5 attempts to login
+            if (RateLimiter::tooManyAttempts($key, 5)) {
+
+                $seconds = RateLimiter::availableIn($key);
+
+                return back()->withErrors([
+                    'email' => "Too many login attempts. Try again in {$seconds} seconds."
                 ]);
+            }
 
-                if (!Auth::attempt($request->only('email', 'password'))) {
-                    $request->session()->regenerate();
-                    //“intended” = the page the user wanted before login
-                    return back()->withErrors(['submit' => 'Invalid credentials.']);
-                    
-                }
 
-                return redirect()->intended('/dashboard');
+            if (!Auth::attempt($request->only('email', 'password'))) {
+                $request->session()->regenerate();
+
+                RateLimiter::hit($key, 120);
+
+                return back()->withErrors(['submit' => 'Invalid credentials.']);
+                
+            }
+
+            RateLimiter::clear($key);
+            $request->session()->regenerate();
+
+            return redirect()->intended('/dashboard');
+
+            RateLimiter::hit($key, 60);
+
+
             } catch (QueryException $e) {
                     logger()->error($e->getMessage());
             } catch (Throwable $e) {
                     logger()->critical($e->getMessage());
             }
-
-        return back();
 
     }
 }
