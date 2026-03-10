d8a286140bdaa7dbd18356828a6a4f254600ca45
Pub De <ppubbuddu@gmail.com>
2026-03-11 00:36:19 +0530

    test: remove test files

--- tests/Feature/AddEmployeeTest.php
+++ tests/Feature/AddEmployeeTest.php
@@ -34,155 +34,155 @@ class AddEmployeeTest extends TestCase
     }
 
 
-    public function test_guest_cannot_view_add_employee()
-    {
+    // public function test_guest_cannot_view_add_employee()
+    // {
 
-        $response = $this->get('/add');
-        $response->assertRedirect('/'); //Default landing page which includes login
-    }
+    //     $response = $this->get('/add');
+    //     $response->assertRedirect('/'); //Default landing page which includes login
+    // }
 
-    public function test_guest_cannot_add_employee()
-    {
-        $response = $this->post(self::ROUTE,[
-            'name' => self::VALID_NAME,
-            'email' => self::VALID_EMAIL,
-            'age' => self::VALID_AGE,
-            'phone' => self::VALID_PHONE,
-            'dob' => self::VALID_DOB,
-            'role' => self::VALID_ROLE,
-        ]);
-
-
-        $response->assertRedirect('/');
-        $this->assertDatabaseCount('employees', 0);
-    }
-
-    public function test_user_can_add_employee_with_valid_data()
-    {
-        $user = User::factory()->create();
-        $response = $this->actingAs($user)->post(self::ROUTE,[
-            'name' => self::VALID_NAME,
-            'email' => self::VALID_EMAIL,
-            'age' => self::VALID_AGE,
-            'phone' => self::VALID_PHONE,
-            'dob' => self::VALID_DOB,
-            'role' => self::VALID_ROLE,
-        ]);
-
-        $response->assertRedirect('/dashboard');
-        $this->assertDatabaseHas('employees', [
-            'email' => self::VALID_EMAIL,
-            'name' => self::VALID_NAME,
-        ]);
-    }
+    // public function test_guest_cannot_add_employee()
+    // {
+    //     $response = $this->post(self::ROUTE,[
+    //         'name' => self::VALID_NAME,
+    //         'email' => self::VALID_EMAIL,
+    //         'age' => self::VALID_AGE,
+    //         'phone' => self::VALID_PHONE,
+    //         'dob' => self::VALID_DOB,
+    //         'role' => self::VALID_ROLE,
+    //     ]);
 
 
-    public function test_add_employee_fails_without_name()
-    {
-        $user = User::factory()->create();
-        $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['name' => '']));
+    //     $response->assertRedirect('/');
+    //     $this->assertDatabaseCount('employees', 0);
+    // }
 
-        $response->assertSessionHasErrors('name');
-        $this->assertDatabaseCount('employees', 0);
-    }
+    // public function test_user_can_add_employee_with_valid_data()
+    // {
+    //     $user = User::factory()->create();
+    //     $response = $this->actingAs($user)->post(self::ROUTE,[
+    //         'name' => self::VALID_NAME,
+    //         'email' => self::VALID_EMAIL,
+    //         'age' => self::VALID_AGE,
+    //         'phone' => self::VALID_PHONE,
+    //         'dob' => self::VALID_DOB,
+    //         'role' => self::VALID_ROLE,
+    //     ]);
 
-
-    public function test_add_employee_fails_without_email()
-    {
-        $user = User::factory()->create();
-        $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['email' => '']));
-
-        $response->assertSessionHasErrors('email');
-        $this->assertDatabaseCount('employees', 0);
-    }
-
-
-    public function test_add_employee_fails_without_age()
-    {
-        $user = User::factory()->create();
-        $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['age' => '']));
-
-        $response->assertSessionHasErrors('age');
-        $this->assertDatabaseCount('employees', 0);
-    }
-
-    public function test_add_employee_fails_without_phone()
-    {
-        $user = User::factory()->create();
-        $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['phone' => '']));
-
-        $response->assertSessionHasErrors('phone');
-        $this->assertDatabaseCount('employees', 0);
-    }
-
-    public function test_add_employee_fails_without_dob()
-    {
-        $user = User::factory()->create();
-        $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['dob' => '']));
-
-        $response->assertSessionHasErrors('dob');
-        $this->assertDatabaseCount('employees', 0);
-    }
-
-    public function test_add_employee_fails_without_role()
-    {
-        $user = User::factory()->create();
-        $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['role' => '']));
-
-        $response->assertSessionHasErrors('role');
-        $this->assertDatabaseCount('employees', 0);
-    }
-
-    public function test_add_employee_name_validation()
-    {
-        $user = User::factory()->create();
-
-        // Name too short
-        $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['name' => 'Jo']));
-        $response->assertSessionHasErrors('name');
-
-        // Name too long
-        $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['name' => str_repeat('A', 16)]));
-        $response->assertSessionHasErrors('name');
-
-        // Name with invalid characters
-        $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['name' => 'John123']));
-        $response->assertSessionHasErrors('name');
-    }
-
-    public function test_add_employee_email_must_be_unique()
-    {
-        $user = User::factory()->create();
-
-        Employee::create([
-            'name' => 'Test User',
-            'email' => self::VALID_EMAIL,
-            'age' => 25,
-            'phone' => '1234567890',
-            'dob' => '2000-01-01',
-            'role' => 'Manager',
-        ]);
+    //     $response->assertRedirect('/dashboard');
+    //     $this->assertDatabaseHas('employees', [
+    //         'email' => self::VALID_EMAIL,
+    //         'name' => self::VALID_NAME,
+    //     ]);
+    // }
 
 
-        $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload());
-        $response->assertSessionHasErrors('email');
-    }
+    // public function test_add_employee_fails_without_name()
+    // {
+    //     $user = User::factory()->create();
+    //     $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['name' => '']));
 
-    public function test_add_employee_age_max_3_digits()
-    {
-        $user = User::factory()->create();
+    //     $response->assertSessionHasErrors('name');
+    //     $this->assertDatabaseCount('employees', 0);
+    // }
 
-        $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['age' => 1234]));
-        $response->assertSessionHasErrors('age');
-    }
 
-    public function test_add_employee_phone_max_10_digits()
-    {
-        $user = User::factory()->create();
+    // public function test_add_employee_fails_without_email()
+    // {
+    //     $user = User::factory()->create();
+    //     $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['email' => '']));
 
-        $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['phone' => '12345678901']));
-        $response->assertSessionHasErrors('phone');
-    }
+    //     $response->assertSessionHasErrors('email');
+    //     $this->assertDatabaseCount('employees', 0);
+    // }
+
+
+    // public function test_add_employee_fails_without_age()
+    // {
+    //     $user = User::factory()->create();
+    //     $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['age' => '']));
+
+    //     $response->assertSessionHasErrors('age');
+    //     $this->assertDatabaseCount('employees', 0);
+    // }
+
+    // public function test_add_employee_fails_without_phone()
+    // {
+    //     $user = User::factory()->create();
+    //     $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['phone' => '']));
+
+    //     $response->assertSessionHasErrors('phone');
+    //     $this->assertDatabaseCount('employees', 0);
+    // }
+
+    // public function test_add_employee_fails_without_dob()
+    // {
+    //     $user = User::factory()->create();
+    //     $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['dob' => '']));
+
+    //     $response->assertSessionHasErrors('dob');
+    //     $this->assertDatabaseCount('employees', 0);
+    // }
+
+    // public function test_add_employee_fails_without_role()
+    // {
+    //     $user = User::factory()->create();
+    //     $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['role' => '']));
+
+    //     $response->assertSessionHasErrors('role');
+    //     $this->assertDatabaseCount('employees', 0);
+    // }
+
+    // public function test_add_employee_name_validation()
+    // {
+    //     $user = User::factory()->create();
+
+    //     // Name too short
+    //     $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['name' => 'Jo']));
+    //     $response->assertSessionHasErrors('name');
+
+    //     // Name too long
+    //     $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['name' => str_repeat('A', 16)]));
+    //     $response->assertSessionHasErrors('name');
+
+    //     // Name with invalid characters
+    //     $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['name' => 'John123']));
+    //     $response->assertSessionHasErrors('name');
+    // }
+
+    // public function test_add_employee_email_must_be_unique()
+    // {
+    //     $user = User::factory()->create();
+
+    //     Employee::create([
+    //         'name' => 'Test User',
+    //         'email' => self::VALID_EMAIL,
+    //         'age' => 25,
+    //         'phone' => '1234567890',
+    //         'dob' => '2000-01-01',
+    //         'role' => 'Manager',
+    //     ]);
+
+
+    //     $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload());
+    //     $response->assertSessionHasErrors('email');
+    // }
+
+    // public function test_add_employee_age_max_3_digits()
+    // {
+    //     $user = User::factory()->create();
+
+    //     $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['age' => 1234]));
+    //     $response->assertSessionHasErrors('age');
+    // }
+
+    // public function test_add_employee_phone_max_10_digits()
+    // {
+    //     $user = User::factory()->create();
+
+    //     $response = $this->actingAs($user)->post(self::ROUTE, $this->validPayload(['phone' => '12345678901']));
+    //     $response->assertSessionHasErrors('phone');
+    // }
 
 
 }
